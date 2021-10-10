c2a5cdef222aca4fdb9126c5db6c23ac4a6c4ce7
wpots <12676504+wpots@users.noreply.github.com>
2021-10-10 12:15:44 +0200

    init

---
+++ app/src/components/LoginWidget.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    Login here
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginWidget',
+
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app/src/views/Portal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="portal">
+    <LoginWidget />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LoginWidget from '@/components/LoginWidget.vue';
+
+export default defineComponent({
+  name: 'Portal',
+  components: {
+    LoginWidget,
+  },
+});
+</script>

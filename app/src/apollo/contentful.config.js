deae3c08b966564b60ba81b8c2f47f83096b5838
wpots <12676504+wpots@users.noreply.github.com>
2021-10-11 22:05:04 +0200

    APIM for contentful

--- app/src/apollo/contentful.config.js
+++ app/src/apollo/contentful.config.js
@@ -1,25 +1,18 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
-import { setContext } from '@apollo/client/link/context';
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.VUE_APP_CONTENTFUL_SPACE_ID}`,
+  uri: '/.netlify/functions/contentful',
+  // uri: '/api/v1/content',
 });
-
-const authLink = setContext((_, { headers }) => ({
-  headers: {
-    ...headers,
-    authorization: `Bearer ${process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN}`,
-  },
-}));
 
 // Cache implementation
 const cache = new InMemoryCache();
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache,
 });
 

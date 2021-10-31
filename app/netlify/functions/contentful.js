4fa69a2f5938916d0887dc0d48b889343eb177e1
wpots <12676504+wpots@users.noreply.github.com>
2021-10-31 14:39:08 +0100

    style loader

--- app/netlify/functions/contentful.js
+++ app/netlify/functions/contentful.js
@@ -2,29 +2,28 @@
 import fetch from 'node-fetch';
 
 export async function handler(event, context) {
-// exports.handler = async (event, context) => {
-const url = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
+  const url = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
 
-try {
-  return await fetch(url, {
-    method: 'POST',
-    body: event.body,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`
-    }
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    return {
-      statusCode:200,
-      body: JSON.stringify(data),
-    }
-  })
-} catch (error) {
+  try {
+    return await fetch(url, {
+      method: 'POST',
+      body: event.body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data),
+        };
+      });
+  } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify(event.body),
-    }
+    };
   }
 }

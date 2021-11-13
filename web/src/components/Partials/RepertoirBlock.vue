57c0096af445af38948e8a53b9d77f8304c24a35
wpots <12676504+wpots@users.noreply.github.com>
2021-11-13 16:36:48 +0100

    repertoire dynamic

--- web/src/components/Partials/RepertoirBlock.vue
+++ web/src/components/Partials/RepertoirBlock.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="section repertoirblock" data-anchor="het-repertoire">
+  <div class="section repertoirblock">
     <div class="container-fluid">
-      <article class="flex-box" id="content-het-repertoire">
+      <article class="flex-box">
         <div class="page-header">
-          <h1>Het Repertoire</h1>
+          <h1>{{ cms.title }}</h1>
         </div>
 
-        <div class="list list--figure row" data-filter="">
-          <Sortable />
-          <RepertoirList />
+        <div class="list list--figure row">
+          <Sortable :items="filterGenres" />
+          <RepertoirList :songs="featuredSongs" />
         </div>
       </article>
+    </div>
+    <div v-if="error">
+      {{ error.value }}
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
+<script>
+import { ref, defineComponent, onErrorCaptured } from 'vue';
+import { useQuery, useResult } from '@vue/apollo-composable';
+import { getRepertoirBlock } from './repertoirBlock.graphql';
 import RepertoirList from '../RepertoirList.vue';
 import Sortable from '../Sortable.vue';
 
@@ -25,5 +29,36 @@ import Sortable from '../Sortable.vue';
 export default defineComponent({
   name: 'RepertoirBlock',
   components: { RepertoirList, Sortable },
+  props: {
+    cms: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  setup() {
+    const error = ref({});
+    const { result } = useQuery(getRepertoirBlock, { anchor: 'ons-repertoire' });
+    const featuredSongs = useResult(
+      result,
+      null,
+      (data) => data.repertoirBlockCollection.items[0].songsCollection.items,
+    );
+    const filterGenres = useResult(result, null, (data) => {
+      const allGenres = data.muziekGenresCollection.items;
+      const usedGenres = allGenres
+        .filter((genre) => genre.linkedFrom.scoreCollection.total > 0)
+        .map((i) => {
+          return { genre: i.genre, total: i.linkedFrom.scoreCollection.total };
+        });
+      return usedGenres;
+    });
+    onErrorCaptured((e) => {
+      error.value = e;
+      return true;
+    });
+    return { filterGenres, featuredSongs, error };
+  },
 });
 </script>
--- web/src/components/Sortable.vue
+++ web/src/components/Sortable.vue
@@ -3,7 +3,7 @@
     <a
       data-group="a-capella"
       class="tag-cloud-link tag-link-12 tag-link-position-1"
-      style="font-size: 12.2857pt; opacity: 1;"
+      style="font-size: 12.2857pt; opacity: 1"
       aria-label="A Capella (3 items)"
       data-animate="bounceIn"
       >A Capella</a
@@ -11,7 +11,7 @@
     <a
       data-group="ballads"
       class="tag-cloud-link tag-link-17 tag-link-position-2"
-      style="font-size: 14.8571pt; opacity: 1;"
+      style="font-size: 14.8571pt; opacity: 1"
       aria-label="Ballads (5 items)"
       data-animate="bounceIn"
       >Ballads</a
@@ -19,7 +19,7 @@
     <a
       data-group="engels"
       class="tag-cloud-link tag-link-18 tag-link-position-3"
-      style="font-size: 22pt; opacity: 1;"
+      style="font-size: 22pt; opacity: 1"
       aria-label="Engels (18 items)"
       data-animate="bounceIn"
       >Engels</a
@@ -27,7 +27,7 @@
     <a
       data-group="frans"
       class="tag-cloud-link tag-link-15 tag-link-position-4"
-      style="font-size: 8pt; opacity: 1;"
+      style="font-size: 8pt; opacity: 1"
       aria-label="Frans (1 item)"
       data-animate="bounceIn"
       >Frans</a
@@ -35,7 +35,7 @@
     <a
       data-group="kerst"
       class="tag-cloud-link tag-link-20 tag-link-position-5"
-      style="font-size: 15.8571pt; opacity: 1;"
+      style="font-size: 15.8571pt; opacity: 1"
       aria-label="Kerst (6 items)"
       data-animate="bounceIn"
       >Kerst</a
@@ -43,7 +43,7 @@
     <a
       data-group="nederlands"
       class="tag-cloud-link tag-link-19 tag-link-position-6"
-      style="font-size: 14.8571pt; opacity: 1;"
+      style="font-size: 14.8571pt; opacity: 1"
       aria-label="Nederlands (5 items)"
       data-animate="bounceIn"
       >Nederlands</a
@@ -51,7 +51,7 @@
     <a
       data-group="pop"
       class="tag-cloud-link tag-link-21 tag-link-position-7"
-      style="font-size: 13.7143pt; opacity: 1;"
+      style="font-size: 13.7143pt; opacity: 1"
       aria-label="Pop (4 items)"
       data-animate="bounceIn"
       >Pop</a
@@ -59,10 +59,23 @@
     <a
       data-group="uptempo"
       class="tag-cloud-link tag-link-16 tag-link-position-8"
-      style="font-size: 18.5714pt; opacity: 1;"
+      style="font-size: 18.5714pt; opacity: 1"
       aria-label="Uptempo (10 items)"
       data-animate="bounceIn"
       >Uptempo</a
     >
   </div>
 </template>
+<script>
+export default {
+  name: 'Sortable',
+  props: {
+    items: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+};
+</script>

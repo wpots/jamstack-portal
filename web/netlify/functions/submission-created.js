166e1806d3fac9149bc2d455acaee2c1dccff71d
wpots <12676504+wpots@users.noreply.github.com>
2021-11-16 21:58:15 +0100

    fix overflow in carousel on mobile

--- web/netlify/functions/submission-created.js
+++ web/netlify/functions/submission-created.js
@@ -1,3 +1,27 @@
+import sgMail from '@sendgrid/mail';
 export async function handler(event, context) {
-  return { statusCode: 200 };
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  const data = JSON.parse(event.body).payload.data;
+  console.log(`Received a submission:`, data);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'website@goedgebekt.com', // Change to your recipient
+    from: data.email, // Change to your verified sender
+    subject: data.subject,
+    text: data.naam + ' : ' + data.message,
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+      return {
+        statusCode: 200,
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+      return { statusCode: 418 };
+    });
 }

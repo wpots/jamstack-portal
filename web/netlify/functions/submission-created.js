a46896865fd080abc515bfd17c0b36a1f414592c
wpots <12676504+wpots@users.noreply.github.com>
2021-11-16 22:57:26 +0100

    submission entry

--- web/netlify/functions/submission-created.js
+++ web/netlify/functions/submission-created.js
@@ -7,21 +7,25 @@ export async function handler(event, context) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: 'website@goedgebekt.com', // Change to your recipient
-    from: data.email, // Change to your verified sender
-    subject: data.subject,
-    text: data.naam + ' : ' + data.message,
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    from: 'website@goedgebekt.com', // Change to your verified sender
+    subject: `Contactformulier Goedgebekt.com: ${data.subject}`,
+    text: data.naam + ' : ' + data.message + 'from: ' + data.email,
+    html: `<p><b>Name: </b>${data.naam}</p>
+                        <p><b>Email: </b>${data.email}</p>
+                        <p><b>Message: </b>${data.message}</p>`,
   };
   sgMail
     .send(msg)
-    .then(() => {
+    .then((data) => {
       console.log('Email sent');
       return {
         statusCode: 200,
+        body: JSON.stringify(data),
       };
     })
     .catch((error) => {
       console.error(error);
-      return { statusCode: 418 };
+      return { statusCode: 418, body: JSON.stringify(error) };
     });
+  return { statusCode: 200 };
 }
